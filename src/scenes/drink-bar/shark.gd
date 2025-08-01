8a0c2ea639e1de06a92649aa32a6bd30a5502efb
lunaru100 <lunaruteyvat@gmail.com>
2025-08-01 19:14:17 +0200

    added drink bar mini game

---
+++ src/scenes/drink-bar/shark.gd
@@ -0,0 +1,57 @@
+extends Area2D
+
+var moving := false
+var speed := 500
+var chosen_drink = null
+
+@export var drink_indicator_scene : PackedScene
+
+var drink_indicator_instance = null
+
+func _ready():
+	randomize()
+	
+	var viewport_size = get_viewport_rect().size
+	position = Vector2(-800, 0)
+
+	var shark_size = Vector2(667, 595)
+	var tween = create_tween()
+	tween.tween_property(self, "position", Vector2(0 , 0), 1.0)
+	
+	var drinks = get_tree().get_nodes_in_group("Drinks")
+	if drinks.size() == 0:
+		print("No drinks")
+		return
+		
+	chosen_drink = drinks[randi() % drinks.size()]
+	
+	if drink_indicator_scene:
+		drink_indicator_instance = drink_indicator_scene.instantiate()
+		add_child(drink_indicator_instance)
+		drink_indicator_instance.position = Vector2(300, -300)
+
+		var drink_sprite = chosen_drink.get_node_or_null("Sprite2D")
+		if drink_sprite:
+			drink_indicator_instance.set_drink_texture(drink_sprite.texture)
+	
+	connect("area_entered", Callable(self, "_on_Shark_area_entered"))
+
+func _process(delta):
+	if moving:
+		position.x += speed * delta
+		if position.x > get_viewport_rect().size.x + 150:
+			queue_free()
+
+func start_moving():
+	moving = true
+		
+func _on_drink_dropped(drink):
+	if drink == chosen_drink:
+		if drink_indicator_instance:
+			drink_indicator_instance.queue_free()
+		start_moving()
+		drink.queue_free()
+	else:
+		var game_over = get_node("../GameOver")
+		game_over.visible = true
+		print("YOU DIED\nGAME OVER")	
