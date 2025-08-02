55c576d04a3292ab61484d4d4ee9e349a8905baf
kuba <jakub.kopaniewski@protonmail.com>
2025-08-02 07:55:10 +0200

    mostly done

--- src/scenes/levels/level.gd
+++ src/scenes/levels/level.gd
@@ -5,3 +5,7 @@ var shark = preload("res://src/scenes/shark/shark.tscn")
 func _ready() -> void:
 	var shark_instance = shark.instantiate()
 	$Sharks.add_child(shark_instance)
+
+
+func _on_player_fish_interact() -> void:
+	$Sharks.get_child(0).get_node("Need").queue_free()
