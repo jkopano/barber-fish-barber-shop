5c153de856a273ebeef9ef9ba214c2a6b7875d65
kuba <jakub.kopaniewski@protonmail.com>
2025-08-03 10:40:11 +0200

    level popup fixed

--- src/scenes/LvL1/level_changer.gd
+++ src/scenes/LvL1/level_changer.gd
@@ -3,7 +3,9 @@ extends Node2D
 func _ready():
 	var shrek_number = Globals.game.get_current_run().get_current().current_shrek_number
 	var shrek_amount = Globals.game.get_current_run().get_current().shrek_amount
+	var level = Globals.game.get_current_run().current_level
 
 	print(shrek_number, shrek_amount)
 
 	$SharksLeft.text = "Sharks Left " + str( shrek_amount - shrek_number + 1 ) + "/" + str( shrek_amount )
+	$CurrentLevel/Label.text = "Level " + str(level + 1)
